b43318bba6ba82b5e980f4044ab7c56e294d2c76
EdensStudio <83062244+EdensStudio@users.noreply.github.com>
2025-11-24 19:45:18 +0800

    Versioning fix

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 const config = {
-    latestVersion:"1.0.0",
-  name:"Coalition Engine",
-  otherVersions:["0.8.0","0.7.6","0.7.5","0.7.4","0.7.1","0.7.0"],
-  themeColor:"#00CBCB",
+    themeColor:"#00CBCB",
   description:"The modern Undertale engine",
   favicon:"assets/docsIcon.png",
+  latestVersion:"1.3.0",
+  otherVersions:["0.8.0","0.7.6","0.7.5","0.7.4","0.7.1","0.7.0","1.0.0","1.0.11"],
+  name:"Coalition Engine",
 };
